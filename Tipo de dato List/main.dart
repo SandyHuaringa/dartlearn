1a6aa7c0770f1c004bec209d2a29dba2d99e484e
SandyHG <shuaringa99@gmail.com>
2019-08-09 19:00:06 -0500

    complete tipo de date list

---
+++ Tipo de dato List/main.dart
@@ -0,0 +1,9 @@
+main(){
+  var list = [1,2,3,4,5,6];
+  list.add(7);//Es para poder agregar lo que continua
+  print(list);
+  //print(list[0]); el lugar de 0 muestra  1
+  //print(list.length) // para saber cuantos elementos tiene
+  //print(list.last); // para acceder al ultimo elemento
+  //print(list.first); // para acceder al primer elemento
+}
